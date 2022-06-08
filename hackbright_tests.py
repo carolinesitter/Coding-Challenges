14f3fd7da1a29d4bdfedbbbebf70c7016d9553db
Caroline <carolinesitter24@gmail.com>
2022-06-08 14:11:57 -0500

    Renamed Files Again

---
+++ hackbright_tests.py
@@ -0,0 +1,14 @@
+import hackbright-challenges
+import unittest
+
+class PrintDigitsBackwards(unittest.TestCase):
+    """Test that the solution to print digits backwards is correct,
+        and that the digits indeed print backwards."""
+    
+    def test_print_digits(self):
+        assertIs(314, 413)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
