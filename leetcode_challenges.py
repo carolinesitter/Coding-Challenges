6c3ae561541c3b3a874753acc2c778e43ce567c5
Caroline <carolinesitter24@gmail.com>
2022-06-08 15:26:14 -0500

    Began Solving Two Sum in leetcode_challenges.py

---
+++ leetcode_challenges.py
@@ -0,0 +1,16 @@
+# CHALLENGE 1: TWO SUM
+
+## Given an array of intergers "nums" and an interger "target", return the INDICES
+## of the two numbers that add up to the target. 
+## You may assume that each input would have exactly one solution, and you may not
+## use the same element twice. 
+## You can return it in any order.
+
+# Test Case:
+## >>> nums = [3,2,6,8]
+## >>> target = 9
+## [0,2]   (Because nums[0] + nums[2] == 9)
+
+# Pseudocode:
+## define a function that takes in a list of numbers, and a target
+## create an empty dictionary to store the values of what we have checked
