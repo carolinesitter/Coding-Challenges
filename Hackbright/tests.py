5af068167b3897c88ff75e229a6b3d86a084616d
Caroline Sitter <carolinesitter@Carolines-MBP.attlocal.net>
2022-07-14 17:54:27 -0500

    Added Concatenate Lists Test to tests.py

--- Hackbright/tests.py
+++ Hackbright/tests.py
@@ -1,7 +1,8 @@
-import pangram
+import pangram, concatenate_lists
 import unittest
 
 from pangram import is_pangram
+from concatenate_lists import concat_lists
 
 class Pangram(unittest.TestCase):
     """Test if a string is a pangram or not."""
@@ -14,6 +15,12 @@ class Pangram(unittest.TestCase):
 
         # If we insert an actual pangram, we should get a True return value
         assert(is_pangram("Amazingly few discotheques provide jukeboxes.") == True)
+
+class ConcatenateLists(unittest.TestCase):
+    """Test if a list of strings is concatenated together."""
+
+    # Write a test to check if our concat_lists function works
+    assert(concat_lists([1,4,6],[2,3]) == [1,4,6,2,3])
 
 if __name__ == '__main__':
     unittest.main()
