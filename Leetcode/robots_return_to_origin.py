49289e2d15949bce410d71f95b13e1a6c2731a5b
Caroline Sitter <carolinesitter24@gmail.com>
2022-09-22 13:46:59 -0500

    Solved robots_return_to_origin.py

--- Leetcode/robots_return_to_origin.py
+++ Leetcode/robots_return_to_origin.py
@@ -23,3 +23,42 @@
 ## Output: false (robot does not return to origin after it finishes all of its moves)
 
 
+# PSEUDOCODE:
+# ------------
+## create a class for robots 
+## within this class, write a function that takes in the robot(self) and the moves
+## establish the orgin of the 2d plane the be 0,0 by setting x and y to 0
+## then, iterate through the string moves and for each letter, assign the x and y values
+## to establish where the robot will move on the 2d plane.
+## Return true if the final placement is 0,0, and false otherwise. 
+
+
+# CODE:
+# -----
+class Robot(object):
+    def return_to_origin(self, moves):
+
+        # set origin coordinates to 0,0
+        x = 0
+        y = 0
+
+        # check each move 
+        for move in moves:
+            if move == "U":
+                y += 1
+            elif move == "D":
+                y -= 1
+            elif move == "R":
+                x += 1
+            elif move == "L":
+                y -= 1
+
+        # return true if the final placement is 0,0, and false otherwise
+        # statement is truthy
+        return x == 0 and y == 0
+
+
+# RUNTIME COMPLEXITY:
+# -------------------
+## This solution has an O(N) or linear runtime complexity as it is only
+## looping through the "moves" string once. 
