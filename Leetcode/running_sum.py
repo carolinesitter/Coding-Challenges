090f7354bd4b4c230d3d3702e5c3c4aa7bd02926
Caroline Sitter <carolinesitter24@gmail.com>
2022-09-22 14:16:02 -0500

    Solved running_sum.py

---
+++ Leetcode/running_sum.py
@@ -0,0 +1,43 @@
+# CHALLENGE: RUNNING SUM OF 1D ARRAY
+# -----------------------------------
+## Given an array nums. We define a running sum of an array as 
+## runningSum[i] = sum(nums[0]…nums[i]).
+
+## Return the running sum of nums.
+
+# TEST CASE 1:
+# ------------
+## Input: [1,2,3,4]
+## Output: [1,3,6,10]
+
+# TEST CASE 2:
+# ------------
+## Input: [1,1,1,1,1,]
+## Output: [1,2,3,4,5]
+
+# TEST CASE 3:
+# ------------
+## Input: [1,4,5,8]
+## Output: [1,5,9,13]
+
+# PSEUDOCODE:
+# ------------
+## write a function that takes in an array of nums
+## create an empty list and assign it to a variable called running_sum
+## write a for loop that iterates through each number,
+## and adds the previous number in the array.
+## add this sum to the new list (append)
+
+# CODE:
+# -----
+class RunSum(object):
+    def get_running_sum(self, nums):
+
+        running_sum = []
+        temp_sum = 0
+
+        for num in nums:
+            temp_sum += num
+            running_sum.append(temp_sum)
+
+        return running_sum
